a3c2a7639ba32b9ac157410926e6e89731d3f90f
Niseiyen <127323783+Niseiyen@users.noreply.github.com>
2024-11-14 19:16:45 -0500

    On way collision Test

--- godot-test/Scenes/ObjectReutilisable/Script/player.gd
+++ godot-test/Scenes/ObjectReutilisable/Script/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -250.0
+const JUMP_VELOCITY = -350.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,7 +12,10 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		print("L'utilisateur a sauté")
-
+	
+	if Input.is_action_just_pressed("move_down") and is_on_floor():
+		position.y += 1.5	
+	
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
